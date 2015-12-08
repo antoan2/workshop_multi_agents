0696c76e77315965e6eafac5464f1a75964ad3bb
antoan2 <antoine.biard.10@gmail.com>
2015-12-08 17:08:04 +0100

    small updates

--- agent.py
+++ agent.py
@@ -14,26 +14,26 @@ class agent(object):
         self.id = self.canvas.create_oval(self.getBox(5),
                                 outline='red',
                                 fill='blue')
-        self.r_eye_id = self.canvas.create_oval(self.getBox(2, (-2, -4)),
-                                outline='red',
-                                fill='blue')
-        self.l_eye_id = self.canvas.create_oval(self.getBox(2, (2, -4)),
-                                outline='red',
-                                fill='blue')
+        #self.r_eye_id = self.canvas.create_oval(self.getBox(2, (-2, -4)),
+                                #outline='red',
+                                #fill='blue')
+        #self.l_eye_id = self.canvas.create_oval(self.getBox(2, (2, -4)),
+                                #outline='red',
+                                #fill='blue')
 
         self.W = self.canvas.winfo_reqwidth()
         self.H = self.canvas.winfo_reqheight()
 
     def move(self):
-        self.speed.rot(0.5*(2*random.random() - 1))
+        self.speed.rot(0.8*(2*random.random() - 1))
         self.pos.add(self.speed.multiply(100*self.dt))
         self.pos.modulo((self.W, self.H))
         self.draw()
     
     def draw(self):
         self.canvas.coords(self.id, self.getBox(5))
-        self.canvas.coords(self.r_eye_id, self.getBox(2, (-2, 4)))
-        self.canvas.coords(self.l_eye_id, self.getBox(2, (2, 4)))
+        #self.canvas.coords(self.r_eye_id, self.getBox(2, (-2, 4)))
+        #self.canvas.coords(self.l_eye_id, self.getBox(2, (2, 4)))
 
     def getBox(self, r, (tx, ty)=(0, 0)):
         return (self.pos.x-r + tx, self.pos.y-r + ty,
